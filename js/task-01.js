9a30ea2f59ce167de82b9e692e1486093e84a5eb
Jakub Rozga <jakub.rozga@tlen.pl>
2023-02-19 16:34:04 +0100

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+
+const categoryCount = categories.querySelectorAll('.item').length;
+console.log(`Number of categories: ${categoryCount}`);
+
+
+const items = document.querySelectorAll('.item');
+items.forEach(item => {
+  const itemCount = item.querySelectorAll('li').length;
+  console.log(`Category: ${item.querySelector('h2').textContent} \n Elements: ${itemCount}`);
+});
