2415e01ca250cbf1c55b96cd6f9e0b9854e66b2c
Jakub Rozga <jakub.rozga@tlen.pl>
2023-02-20 21:12:22 +0100

    zprettierizowane

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,12 @@
-
-const categoryCount = categories.querySelectorAll('.item').length;
+const categoryCount = categories.querySelectorAll(".item").length;
 console.log(`Number of categories: ${categoryCount}`);
 
-
-const items = document.querySelectorAll('.item');
-items.forEach(item => {
-  const itemCount = item.querySelectorAll('li').length;
-  console.log(`Category: ${item.querySelector('h2').textContent} \n Elements: ${itemCount}`);
+const items = document.querySelectorAll(".item");
+items.forEach((item) => {
+  const itemCount = item.querySelectorAll("li").length;
+  console.log(
+    `Category: ${
+      item.querySelector("h2").textContent
+    } \n Elements: ${itemCount}`
+  );
 });
