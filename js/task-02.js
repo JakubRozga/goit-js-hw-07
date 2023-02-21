5cae2139aa5f9933662f250d8de6cfa73bd36cfc
Jakub Rozga <jakub.rozga@tlen.pl>
2023-02-21 22:22:56 +0100

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+const ul = document.getElementById("ingredients");
+
+ingredients.forEach(function(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+  ul.appendChild(li);
+});
